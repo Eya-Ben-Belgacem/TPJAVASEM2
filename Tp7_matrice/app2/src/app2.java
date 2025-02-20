34a826f21a042efff5826104671bb05e9807e9bf
Eya-Ben-Belgacem <benbelgacemeyaa@gmail.com>
2025-02-20 12:48:51 +0100

    first commit

---
+++ Tp7_matrice/app2/src/app2.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)app2.java
+ *
+ * app2 application
+ *
+ * @author 
+ * @version 1.00 2025/2/20
+ */
+ 
+public class app2 {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
